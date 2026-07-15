8a4f03136002ef68ca26643df61fa7a76b70f1db
GayatriB01 <addalag@vcu.edu>
2026-07-15 15:53:13 -0400

    Exploring Pandas conditions and fitering

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ data=pd.read_csv("data/books.csv")
 df=pd.DataFrame(data)
 
 print(df.head())
+print(df.shape)
+print(df.columns)
+print(df.info())
+print(df[['Title','Author']])
+print(
+    df[
+      (
+          (df['Genre']=='AI')
+          |
+          (df['Genre']=='programming')
+          
+      )
+      &
+      (df['Year']>2015)
+    ]
+)
